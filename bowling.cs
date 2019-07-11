e114d35f8b1719eb433dd27fcf3eee69e9d7ea2a
tavisca-adsouza <51948054+tavisca-adsouza@users.noreply.github.com>
2019-07-11 14:27:20 +0530

    Add files via upload

---
+++ bowling.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        public static int calculateScore(int[,] fields){
+            int score = 0;
+            //bool strike = false;
+            //sbool spare = false;
+
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine("The two scores for this field are {0} and {1}", fields[i, 0], fields[i, 1]);
+                int firstRollScore = fields[i, 0];
+                int secondRollScore = fields[i, 1];
+                int totalScore = firstRollScore + secondRollScore;
+                
+                if(totalScore > 10){
+                    throw new InvalidCastException();
+                }
+
+                score += totalScore;
+
+                if(firstRollScore == 10){
+                    // the condition where strike = true;
+                    score += fields[i+1, 0] + fields[i+1, 1];
+                }
+                else if(totalScore == 10){
+                    // the conditon where spare = true;
+                    score += fields[i+1, 0];
+                }
+            
+            System.Console.WriteLine("The score till now is: {0}", score);
+
+
+            }
+
+            return score;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //var resultObj = new Statistics();
+
+            
+            int[,] pairScore = {{4, 5}, {10, 0}, {5, 5}, {3, 4}, {4, 5}, {3, 4}, {1, 7}, {4, 4}, {4, 5}, {9, 0}};
+            int result = Program.calculateScore(pairScore);
+            System.Console.WriteLine(result);
+
+            
+        }
+    }
+}
---
+++ bowlingTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //rogram pg = new Program();
+            int[,] pairScore = {{4, 5}, {10, 0}, {5, 5}, {3, 4}, {4, 5}, {3, 4}, {1, 7}, {4, 4}, {4, 5}, {9, 0}};
+
+	        var x = 5;
+            var y = 2;
+            var expected = 7;
+            var actual = x + y + 1;
+            Assert.Equal(expected, actual);
+
+            var result = Program.calculateScore(pairScore);
+
+            Assert.Equal(99, result);
+
+        }
+    }
+}
